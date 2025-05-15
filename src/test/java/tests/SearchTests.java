d8408780bf31e3b12d405e9d6a29bc998164a6ef
Niks1707 <genkel1707@gmail.com>
2025-05-15 18:21:49 +0200

    fix

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -5,13 +5,21 @@ import org.junit.jupiter.api.Test;
 import pages.MainPage;
 import pages.OnboardingPage;
 
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static io.appium.java_client.AppiumBy.accessibilityId;
+import static io.appium.java_client.AppiumBy.id;
+import static io.qameta.allure.Allure.step;
+
 public class SearchTests extends TestBase {
     OnboardingPage onboardingPage = new OnboardingPage();
     MainPage mainPage = new MainPage();
 
     @Test
     @DisplayName("Проверка прохождения онбординга приложения")
-    public void OnboardingTest() {
+    public void onboardingTest() {
         String deviceHost = System.getProperty("deviceHost", "local");
 
         // Если deviceHost не равно "browserstack", то выполняем шаги онбординга
@@ -26,5 +34,5 @@ public class SearchTests extends TestBase {
             onboardingPage.clickGetStartedButton();
         }
         mainPage.checkSuccessfulOpenMainPage();
-    }
+    }    
 }
